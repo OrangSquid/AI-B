a00bdbf0ba8d70e7defcdf3b00c47142c4075e6c
OrangSquid <rafael.dom.matos@gmail.com>
2021-01-21 10:00:38 +0000

    Ex. 2 da Ficha de Revisões 2

--- Ficha Revisões 2/ProblemaR2.2.c
+++ Ficha Revisões 2/ProblemaR2.2.c
@@ -3,21 +3,22 @@
 
 int main() {
 	char texto[188], palavra_comprida[188] = "", palavra_comparar[188] = "";
-	int contado = 0;
 	gets(texto);
 	for(int i = 0; i < strlen(texto); i++) {
-		if(texto[i] != ' ') {
-			printf("%d\n", i);
+		if(texto[i] != ' ' && !texto[i+1] == '\0') {
 			palavra_comparar[strlen(palavra_comparar)] = texto[i];
-			contado = 0;
 		}
-		else if(texto[i] == ' ' && !contado) {
-			contado = 1;
+		else if(texto[i] != ' ' && texto[i+1] == '\0') {
+			palavra_comparar[strlen(palavra_comparar)] = texto[i];
 			if(strlen(palavra_comprida) < strlen(palavra_comparar)) {
 				strcpy(palavra_comprida, palavra_comparar);
-				printf("%s\n", palavra_comparar);
-				char palavra_comparar[188] = "\0";
-				printf("%s\n", palavra_comparar);
+				memset(palavra_comparar, 0, strlen(palavra_comparar));
+			}
+		}
+		else if(texto[i] == ' ' && texto[i-1] != ' ') {
+			if(strlen(palavra_comprida) < strlen(palavra_comparar)) {
+				strcpy(palavra_comprida, palavra_comparar);
+				memset(palavra_comparar, 0, strlen(palavra_comparar));
 			}
 		}
 	}
